09e13b43d14f6b03df89cc35b05d280105b53cc8
copiousfreetime <jeremy@copiousfreetime.org>
2021-08-20 17:20:55 -0600

    update big sync job to be smarter

--- app/jobs/freshbooks_time_entries_push_job.rb
+++ app/jobs/freshbooks_time_entries_push_job.rb
@@ -3,7 +3,15 @@ class FreshbooksTimeEntriesPushJob < FreshbooksSyncJob
 
   def perform(*args)
     ::TimeEntry.for_freshbooks.each do |time_entry|
-      FreshbooksTimeEntryPushJob.new.push_time_entry(time_entry)
+      if time_entry.needs_freshbooks_push? then
+        Rails.logger.info "Pushing time entry #{time_entry.id} #{time_entry.freshbooks_time_entry.sync_state}"
+        FreshbooksTimeEntryPushJob.new.push_time_entry(time_entry)
+      end
+    end
+
+    ::FreshbooksTimeEntry.pending_delete.each do |freshbooks_time_entry|
+      Rails.logger.info "Deleging freshbooks time entry #{freshbooks_time_entry.id} #{freshbooks_time_entry.sync_state}"
+      FreshbooksTimeEntryDeleteJob.new.delete_freshbooks_time_entry(freshbooks_time_entry)
     end
   end
 end
